ac0b6f7ad9ae79d32a314968f323d3c5b1f751aa
Yousef Alsaadany <58972656+y0usefalsaadany@users.noreply.github.com>
2021-07-09 03:52:01 +0200

    Update errorInfo.php

--- app/views/error/errorInfo.php
+++ app/views/error/errorInfo.php
@@ -2,7 +2,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - 404 page</title>
+  <title>joe framework - 404 page</title>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Arvo'><link rel="stylesheet" href="./style.css">
 
